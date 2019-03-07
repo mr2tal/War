8f0c6ad57c68db2928414956209be1f087db79b4
mr2 <nielsrb@outlook.dk>
2019-03-07 18:56:26 +0100

    added card and deck scripts

---
+++ War/Assets/Scripts/Cards.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cards : MonoBehaviour
+{
+
+    public enum Suit
+    {
+        diamonds,
+        hearts,
+        clubs,
+        spades
+    }
+
+    public Card twoOfHearts = new Card(Suit.hearts, 2, false, null, null);
+
+    void Start()
+    {
+        print(twoOfHearts._suit);
+    }
+
+    public class Card
+    {
+        //fields
+        public Suit _suit { get; set; }
+        public int _number { get; set; }
+        public bool _isFaceUp { get; set; }
+        public GameObject _faceup { get; set; }
+        public GameObject _facedown { get; set; }
+        //constructor
+        public Card(Suit suit, int number, bool isFaceUp, GameObject faceup, GameObject facedown)
+        {
+            _suit = suit;
+            _number = number;
+            _isFaceUp = isFaceUp;
+            _faceup = faceup;
+            _facedown = facedown;
+        }
+    }
+}
---
+++ War/Assets/Scripts/Decks.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Decks : MonoBehaviour
+{
+    private Cards cards;
+
+    void Start()
+    {
+        cards = GetComponent<Cards>();
+        Deck preShuffleDeck = new Deck(new List<Cards.Card>());
+
+        preShuffleDeck._deck.Add(cards.twoOfHearts);
+
+        
+
+    }
+    public class Deck
+    {
+
+        //fields
+        public List<Cards.Card> _deck { get; set; }
+
+        //constructor
+        public Deck(List<Cards.Card> deck)
+        {
+            _deck = deck;
+        }
+    }
+}
